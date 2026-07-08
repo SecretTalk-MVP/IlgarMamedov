6698795a318e043f4269a5733cf66cf254b32795
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-08 18:46:43 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
 const axios = require('axios');
 const fs = require('fs');
+const db = require('./database/db');
 
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
