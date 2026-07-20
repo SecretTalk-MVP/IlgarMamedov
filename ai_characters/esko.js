95fe7ad1fee692c8d8e4a599ee85442b6df6ba64
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-21 02:55:29 +0400

    feat(ai): load esko.system.md

--- ai_characters/esko.js
+++ ai_characters/esko.js
@@ -1,3 +1,12 @@
+const fs = require("fs");
+const path = require("path");
+
+const systemPrompt = fs.readFileSync(
+    path.join(__dirname, "esko.system.md"),
+    "utf8"
+);
+
 module.exports = {
-  name: "Esko"
+    name: "Esko",
+    systemPrompt
 };
