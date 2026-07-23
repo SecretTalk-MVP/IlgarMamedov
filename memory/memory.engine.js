33f7c7e0671a5d68706e2143c00e53cdb542d173
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-23 06:14:01 +0400

    Update memory.engine.js

--- memory/memory.engine.js
+++ memory/memory.engine.js
@@ -1,6 +1,37 @@
 class MemoryEngine {
-analyze(memory, userMessage, aiAnswer) {
+
+    analyze(memory, userMessage, aiAnswer) {
+
+        memory = this.analyzeProfile(memory, userMessage);
+
+        memory = this.analyzeConversation(memory, userMessage, aiAnswer);
+
+        memory = this.analyzeRelationship(memory, userMessage);
+
+        return memory;
+    }
+
+
+    analyzeProfile(memory, userMessage) {
+
+        return memory;
+
+    }
+
+
+    analyzeConversation(memory, userMessage, aiAnswer) {
+
+        return memory;
+
+    }
+
+
+    analyzeRelationship(memory, userMessage) {
+
+        return memory;
+
+    }
 
 }
-}
+
 module.exports = MemoryEngine;
