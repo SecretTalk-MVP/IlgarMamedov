57b7578f33efe88e26472189dcd7abda9534fab6
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-29 02:29:09 +0400

    Update start.controller.js

--- controllers/start.controller.js
+++ controllers/start.controller.js
@@ -1,4 +1,28 @@
+const {
+    pushHistory
+} = require('./navigation.controller');
+
 function registerStartController(bot) {
+
+    bot.onText(/\/start/, (msg) => {
+
+        pushHistory(msg.chat.id, 'main');
+
+        bot.sendMessage(
+            msg.chat.id,
+            'Добро пожаловать в SecretTalk 🚀\n\nВыберите действие:',
+            {
+                reply_markup: {
+                    keyboard: [
+                        ['🤖 Поговорить с ИИ', '👥 Найти собеседника'],
+                        ['⚙️ Фильтр поиска']
+                    ],
+                    resize_keyboard: true
+                }
+            }
+        );
+
+    });
 
 }
 
