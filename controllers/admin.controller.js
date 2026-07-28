5fcb59cf586c39196eb4366654fad215c8fbfaae
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-29 03:33:48 +0400

    Update admin.controller.js

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,36 +1,7 @@
 function registerAdminController(bot) {
-  
-  if (msg.text === '/admin') {
-
-  if (msg.from.id !== 1496574112) {
-    bot.sendMessage(
-      msg.chat.id,
-      '⛔ Доступ запрещён.'
-    );
-    return;
-  }
-    pushHistory(msg.chat.id, 'admin');
-
-  bot.sendMessage(
-    msg.chat.id,
-    '👑 Панель администратора',
-    {
-      reply_markup: {
-        keyboard: [
-          ['👥 Онлайн', '📊 Статистика'],
-          ['📢 Рассылка', '💬 Активные чаты'],
-          ['⚙️ Settings'],
-        ],
-        resize_keyboard: true
-      }
-    }
-  );
-
-  return;
-}
 
 }
 
 module.exports = {
-  registerAdminController
+    registerAdminController
 };
