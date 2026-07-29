0655f902a644f98cb1d8800c3356ee0dd1c08a96
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-30 03:47:52 +0400

    Create index.js

---
+++ modules/index.js
@@ -0,0 +1,10 @@
+module.exports = {
+    admin: require("./admin"),
+    analytics: require("./analytics"),
+    broadcast: require("./broadcast"),
+    chats: require("./chats"),
+    moderation: require("./moderation"),
+    settings: require("./settings"),
+    users: require("./users"),
+    verification: require("./verification")
+};
