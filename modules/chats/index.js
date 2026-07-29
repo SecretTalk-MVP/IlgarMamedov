56e393ad520568299bd1c7e4956edb1fcc50338a
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-07-30 03:37:48 +0400

    Update index.js

--- modules/chats/index.js
+++ modules/chats/index.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+    controllers: require("./controllers"),
+    services: require("./services"),
+    repository: require("./repository"),
+    storage: require("./storage"),
+    routes: require("./routes"),
+    research: require("./research")
+};
