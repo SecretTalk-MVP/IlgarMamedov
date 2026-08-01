9b2bcccf21fcdbb41f69bd115f4b4dfa8bab3756
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-02 02:53:11 +0400

    Update permissions.js

--- modules/admin/permissions.js
+++ modules/admin/permissions.js
@@ -1,7 +1,22 @@
 /**
  * SecretTalk
  * Admin Permissions
- * Version: 1.0
+ * Version: 1.1
  */
 
-module.exports = {};
+const ADMIN_IDS = [
+    process.env.ADMIN_ID
+];
+
+/**
+ * Проверка прав администратора
+ * @param {number|string} userId
+ * @returns {boolean}
+ */
+function isAdmin(userId) {
+    return ADMIN_IDS.includes(String(userId));
+}
+
+module.exports = {
+    isAdmin
+};
