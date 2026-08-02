28f87b81176d811f81fbf18136ab15b4e98aee65
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-03 00:01:06 +0400

    Update index.js

--- index.js
+++ index.js
@@ -134,7 +134,12 @@ bot.on('message', async (msg) => {
 console.log("MESSAGE:", msg.from.id);
  // const memory = await memoryService.loadMemory(msg.from.id);
 await saveUser(msg);
-  onlineUsers.add(msg.from.id);
+
+if (await admin.handle(bot, msg)) {
+    return;
+}
+
+onlineUsers.add(msg.from.id);
   console.log("TEXT =", JSON.stringify(msg.text));
 
   if (msg.text === '👥 Онлайн') {
