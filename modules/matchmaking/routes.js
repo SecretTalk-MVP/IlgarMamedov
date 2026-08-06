3adee73af3712c58a31db3456e3552ab5de6ca09
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:47:28 +0400

    Update routes.js

--- modules/matchmaking/routes.js
+++ modules/matchmaking/routes.js
@@ -1,1 +1,17 @@
+const queue = require('./queue');
+const matcher = require('./matcher');
+const dialogs = require('./dialogs');
+const relay = require('./relay');
 
+async function handle(
+    bot,
+    msg,
+    aiUsers
+) {
+
+    return false;
+}
+
+module.exports = {
+    handle
+};
