52950aff7c58da3ce4ca628f65351e645b930ae2
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:40:38 +0400

    Update matcher.js

--- modules/matchmaking/matcher.js
+++ modules/matchmaking/matcher.js
@@ -1,1 +1,32 @@
+const state = require('./state');
+const queue = require('./queue');
+const dialogs = require('./dialogs');
 
+function findPartner(userId, aiUsers) {
+    let partnerId = null;
+
+    while (state.waitingUsers.length > 0) {
+        const candidate = state.waitingUsers.shift();
+
+        if (candidate === userId) {
+            continue;
+        }
+
+        if (aiUsers[candidate]) {
+            continue;
+        }
+
+        partnerId = candidate;
+        break;
+    }
+
+    if (partnerId) {
+        dialogs.connect(userId, partnerId);
+    }
+
+    return partnerId;
+}
+
+module.exports = {
+    findPartner
+};
