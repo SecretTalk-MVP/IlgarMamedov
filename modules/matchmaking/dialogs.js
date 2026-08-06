bbdd3f4f4a10c1743e3f68b4e636ff2e5f4cfd55
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:39:27 +0400

    Update dialogs.js

--- modules/matchmaking/dialogs.js
+++ modules/matchmaking/dialogs.js
@@ -1,1 +1,34 @@
+const state = require('./state');
 
+function connect(user1, user2) {
+    state.dialogs[user1] = user2;
+    state.dialogs[user2] = user1;
+}
+
+function disconnect(userId) {
+    const partnerId = state.dialogs[userId];
+
+    if (!partnerId) {
+        return null;
+    }
+
+    delete state.dialogs[userId];
+    delete state.dialogs[partnerId];
+
+    return partnerId;
+}
+
+function getPartner(userId) {
+    return state.dialogs[userId] || null;
+}
+
+function isInDialog(userId) {
+    return !!state.dialogs[userId];
+}
+
+module.exports = {
+    connect,
+    disconnect,
+    getPartner,
+    isInDialog
+};
