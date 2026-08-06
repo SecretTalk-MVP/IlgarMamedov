bf2a4460d445fb3ec658957f6f0f63342397618f
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:42:46 +0400

    Update filters.js

--- modules/matchmaking/filters.js
+++ modules/matchmaking/filters.js
@@ -1,1 +1,19 @@
+const state = require('./state');
 
+function set(userId, filters) {
+    state.filters[userId] = filters;
+}
+
+function get(userId) {
+    return state.filters[userId] || {};
+}
+
+function clear(userId) {
+    delete state.filters[userId];
+}
+
+module.exports = {
+    set,
+    get,
+    clear
+};
