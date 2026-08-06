a63d56eed0aba4e218d2a4fd84af0f5df18b896a
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:43:32 +0400

    Update keyboard.js

--- modules/matchmaking/keyboard.js
+++ modules/matchmaking/keyboard.js
@@ -1,1 +1,15 @@
+function main() {
+    return {
+        reply_markup: {
+            keyboard: [
+                ['👥 Найти собеседника'],
+                ['❌ Завершить диалог']
+            ],
+            resize_keyboard: true
+        }
+    };
+}
 
+module.exports = {
+    main
+};
