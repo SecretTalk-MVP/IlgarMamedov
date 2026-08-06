e75c77a9d9cfd9916f9d14f2c5d30e33c691fa31
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:38:11 +0400

    Update queue.js

--- modules/matchmaking/queue.js
+++ modules/matchmaking/queue.js
@@ -1,1 +1,25 @@
+const state = require('./state');
 
+function add(userId) {
+    if (!state.waitingUsers.includes(userId)) {
+        state.waitingUsers.push(userId);
+    }
+}
+
+function remove(userId) {
+    const index = state.waitingUsers.indexOf(userId);
+
+    if (index !== -1) {
+        state.waitingUsers.splice(index, 1);
+    }
+}
+
+function has(userId) {
+    return state.waitingUsers.includes(userId);
+}
+
+module.exports = {
+    add,
+    remove,
+    has
+};
