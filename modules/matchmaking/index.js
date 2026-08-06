d2cedb1174ebd5d5f341cb1ef17a9399b2910dd4
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:44:14 +0400

    Update index.js

--- modules/matchmaking/index.js
+++ modules/matchmaking/index.js
@@ -1,1 +1,17 @@
+const state = require('./state');
+const queue = require('./queue');
+const dialogs = require('./dialogs');
+const matcher = require('./matcher');
+const relay = require('./relay');
+const filters = require('./filters');
+const keyboard = require('./keyboard');
 
+module.exports = {
+    state,
+    queue,
+    dialogs,
+    matcher,
+    relay,
+    filters,
+    keyboard
+};
