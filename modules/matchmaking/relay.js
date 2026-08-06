6585e8b7a99c599bd59738e653845d61ebfd89d0
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 03:41:46 +0400

    Update relay.js

--- modules/matchmaking/relay.js
+++ modules/matchmaking/relay.js
@@ -1,1 +1,27 @@
+const dialogs = require('./dialogs');
 
+async function relay(bot, msg, aiUsers) {
+    if (aiUsers[msg.chat.id]) {
+        return false;
+    }
+
+    const partnerId = dialogs.getPartner(msg.chat.id);
+
+    if (!partnerId) {
+        return false;
+    }
+
+    try {
+        await bot.copyMessage(
+            partnerId,
+            msg.chat.id,
+            msg.message_id
+        );
+    } catch (err) {
+        console.log(err);
+    }
+
+    return true;
+}
+
+module.exports = relay;
