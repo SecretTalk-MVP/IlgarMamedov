6a6a8893674de446ee93ec59210bf481cf74ca64
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-07 06:08:41 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,13 +6,3 @@ const db = require('./database/db');
 const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
-const aiUsers = {};
-const memories = {};
-const waitingUsers = [];
-const dialogs = {};
-const users = {};
-const waitingTimers = {};
-let chatHistory = {};
-const onlineUsers = new Set();
-const userHistory = {};
-async function saveUser(msg) {
