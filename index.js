df8a6d7b9a6fb1a076599b818dee4c8af4bd0b43
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-10 06:19:32 +0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 const aiService = new AIService();
-const searchRoutes = require('./search/routes');
+const matchmakingRoutes = require('./matchmaking/routes');
 const aiUsers = {};
 
 console.log("✅ Bot initialized");
