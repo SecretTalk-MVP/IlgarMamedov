e21ccf73998d446974a52e835177bedf3454ef3c
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-11 02:34:40 +0400

    Create users module

--- modules/users/index.js
+++ modules/users/index.js
@@ -1,1 +1,30 @@
+const db = require('../../database/db');
 
+async function saveUser(msg) {
+    try {
+        await db.query(
+            `INSERT INTO users (telegram_id, username, first_name)
+             VALUES ($1, $2, $3)
+             ON CONFLICT (telegram_id)
+             DO UPDATE SET
+                username = EXCLUDED.username,
+                first_name = EXCLUDED.first_name,
+                last_seen = CURRENT_TIMESTAMP`,
+            [
+                msg.from.id,
+                msg.from.username || null,
+                msg.from.first_name || null
+            ]
+        );
+
+        return true;
+
+    } catch (error) {
+        console.error('UsersService saveUser error:', error);
+        return false;
+    }
+}
+
+module.exports = {
+    saveUser
+};
