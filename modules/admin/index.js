d110d64bd9a4349cb699537d9097adcc9acb3303
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-14 02:22:02 +0400

    Update index.js

--- modules/admin/index.js
+++ modules/admin/index.js
@@ -1,15 +1,6 @@
-const statistics = require("./statistics");
-const menu = require("./menu");
-const permissions = require("./permissions");
 const chatRoutes = require("./chat.routes");
 
 class AdminModule {
-
-    constructor() {
-        this.statistics = statistics;
-        this.menu = menu;
-        this.permissions = permissions;
-    }
 
     async handle(bot, msg, aiUsers) {
 
