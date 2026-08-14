4b58d51fa8bb48fc3dbf052e17ad53e4a46cbf6a
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-14 04:16:29 +0400

    Update index.js

--- modules/router/index.js
+++ modules/router/index.js
@@ -4,25 +4,16 @@ const aida = require("../aida");
 
 class Router {
 
-    async handle(bot, msg, aiUsers) {
+    async handle(bot, msg) {
 
-        /*
-         * 1. Администрирование
-         */
-        if (await admin.handle(bot, msg, aiUsers)) {
+        if (await admin.handle(bot, msg, aida.users)) {
             return true;
         }
 
-        /*
-         * 2. Настройки / фильтр поиска
-         */
-        if (await settings.handle(bot, msg, aiUsers)) {
+        if (await settings.handle(bot, msg, aida.users)) {
             return true;
         }
 
-        /*
-         * 3. AiDa
-         */
         if (await aida.handle(bot, msg)) {
             return true;
         }
