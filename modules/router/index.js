82a48792f06b74dac178df44429042ba7b1763d9
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-14 04:18:09 +0400

    Update index.js

--- modules/router/index.js
+++ modules/router/index.js
@@ -6,14 +6,23 @@ class Router {
 
     async handle(bot, msg) {
 
+        /*
+         * 1. Администрирование
+         */
         if (await admin.handle(bot, msg, aida.users)) {
             return true;
         }
 
+        /*
+         * 2. Настройки / фильтр поиска
+         */
         if (await settings.handle(bot, msg, aida.users)) {
             return true;
         }
 
+        /*
+         * 3. AiDa
+         */
         if (await aida.handle(bot, msg)) {
             return true;
         }
