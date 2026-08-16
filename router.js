16ed9ad16bc8ef5201ba1c25aadc0490535bd4d9
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-16 17:52:26 +0400

    Update router.js

--- router.js
+++ router.js
@@ -1,1 +1,27 @@
+const adminRoutes = require('./modules/admin/chat.routes');
+const aida = require('./modules/aida');
 
+class Router {
+
+    async handle(bot, msg) {
+
+        /*
+         * ADMIN
+         *
+         * Первый подключённый модуль
+         * единого Router.
+         */
+        if (await adminRoutes.handle(
+            bot,
+            msg,
+            aida.users
+        )) {
+            return true;
+        }
+
+        return false;
+    }
+
+}
+
+module.exports = new Router();
