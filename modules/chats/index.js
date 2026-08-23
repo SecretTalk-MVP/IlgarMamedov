12ed190623e37c212db77be6fc81ec7ae26ecaf7
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-23 18:51:00 +0400

    Delete modules/chats/index.js

--- modules/chats/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    controllers: require("./controllers"),
-    services: require("./services"),
-    repository: require("./repository"),
-    storage: require("./storage"),
-    routes: require("./routes"),
-    research: require("./research")
-};
