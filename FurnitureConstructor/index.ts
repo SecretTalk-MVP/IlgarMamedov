8920c8d56ba6454becdee9304216edcb86c9b48f
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-08-28 22:44:22 +0400

    Create index.ts

---
+++ FurnitureConstructor/index.ts
@@ -0,0 +1,12 @@
+export {
+  BaseDrawerModule,
+  default as BaseDrawerModuleDefault,
+} from './BaseDrawerModule';
+
+export type {
+  DrawerSystem,
+  BaseDrawerModuleParameters,
+  PartDefinition,
+  DrawerDefinition,
+  BaseDrawerModuleModel,
+} from './BaseDrawerModule';
