7a91f3d0218bf69db82c5824277c4885992a7bec
SecretTalk-MVP <ilgarmamedov857@gmail.com>
2026-09-19 23:45:35 +0400

    Update session.js

--- modules/randomchat/session.js
+++ modules/randomchat/session.js
@@ -5,6 +5,22 @@ function connect(user1, user2) {
     state.sessions.set(user2, user1);
 }
 
+function setDialogId(userId, dialogId) {
+    if (!state.dialogs) {
+        state.dialogs = new Map();
+    }
+
+    state.dialogs.set(userId, dialogId);
+}
+
+function getDialogId(userId) {
+    if (!state.dialogs) {
+        return null;
+    }
+
+    return state.dialogs.get(userId) || null;
+}
+
 function getPartner(userId) {
     return state.sessions.get(userId) || null;
 }
@@ -23,11 +39,18 @@ function disconnect(userId) {
     state.sessions.delete(userId);
     state.sessions.delete(partnerId);
 
+    if (state.dialogs) {
+        state.dialogs.delete(userId);
+        state.dialogs.delete(partnerId);
+    }
+
     return partnerId;
 }
 
 module.exports = {
     connect,
+    setDialogId,
+    getDialogId,
     getPartner,
     isInDialog,
     disconnect
